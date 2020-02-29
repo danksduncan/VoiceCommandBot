18744a169c83d8ee74ca950723a8f20128a108cf
danksduncan <danksduncan@gmail.com>
2020-02-29 11:28:51 -0500

    Request recognizer update - Added to try-function.

--- Form1.cs
+++ Form1.cs
@@ -24,7 +24,7 @@ namespace VoiceCommandBot
 
             try
             {
-
+                rec.RequestRecognizerUpdate();
             }
 
             s.SelectVoiceByHints(VoiceGender.Female);
