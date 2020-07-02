3904db235dd7cd1f8777649db44f1d62b6b8da84
danksduncan <danksduncan@gmail.com>
2020-07-02 10:34:50 -0400

    Added 'next' and 'last' commands.

--- Form1.cs
+++ Form1.cs
@@ -28,7 +28,7 @@ namespace VoiceCommandBot
         {
             SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
             list.Add(new string[] { "hello", "how are you", "what time is it", "what is today", "open google", "wake", "sleep", "restart", "update", "open word document", "close word document",
-            "whats the weather like", "whats the temperature", "hey amy", "minimize", "unminimize", "maximize", "play", "pause", "spotify" });
+            "whats the weather like", "whats the temperature", "hey amy", "minimize", "unminimize", "maximize", "play", "pause", "spotify", "next", "last" });
 
             Grammar gr = new Grammar(new GrammarBuilder(list));
 
